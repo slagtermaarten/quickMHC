609bac4a03705a077530b0f6a54e23495ba7fa46
Maarten Slagter <slagter.maarten@gmail.com>
2019-02-11 22:53:34 +0100

    Added old version of STS for comparison and sanity checks

--- R/STSPredictor.R
+++ R/STSPredictor.R
@@ -70,12 +70,86 @@ load_selfsimilarity_matrix <- function() {
                          0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,
                          0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1),
                        ncol = 21)
-  rownames(scoreMatrix) = c('W', 'F', 'Y', 'I', 'V', 'L', 'M', 'C', 'D', 'E', 'G',
-                            'A', 'P', 'H', 'K', 'R', 'S', 'T', 'N', 'Q', 'X')
-  colnames(scoreMatrix) = c('W', 'F', 'Y', 'I', 'V', 'L', 'M', 'C', 'D', 'E', 'G',
-                            'A', 'P', 'H', 'K', 'R', 'S', 'T', 'N', 'Q', 'X')
+  rownames(scoreMatrix) = c('W', 'F', 'Y', 'I', 'V', 'L', 'M', 'C', 'D', 'E',
+    'G', 'A', 'P', 'H', 'K', 'R', 'S', 'T', 'N', 'Q', 'X')
+  colnames(scoreMatrix) = c('W', 'F', 'Y', 'I', 'V', 'L', 'M', 'C', 'D', 'E',
+    'G', 'A', 'P', 'H', 'K', 'R', 'S', 'T', 'N', 'Q', 'X')
   return(scoreMatrix)
 }
+
+
+#' Match many seqs
+#'
+#' Test whether @param{query_peps} are different from all peptides in
+#' @param{ref_list}. Return T if peptide is different from all peptides in the
+#' ref list
+#'
+match_seq_ext <- function(query_peps, ref_list,
+  scorematrix = load_self_sim_mat()) {
+  if (!is.character(query_peps))
+    stop('Input query_peps is supposed to be a character vector')
+  if (!is.character(ref_list))
+    stop('Input ref_list is supposed to be a character vector')
+
+  match_seq_ext_comp <- compiler::cmpfun(match_single_seq_ext)
+  vapply(query_peps, function(query_pep) {
+    all(vapply(ref_list,
+        function(ref_seq) match_seq_ext_comp(seq1 = query_pep,
+          seq2 = ref_seq, scorematrix = scorematrix)$keep.in.list,
+    logical(1)))
+  }, logical(1))
+}
+
+
+#' Compare two peptide sequences for similarity
+#'
+#'
+match_single_seq_ext <- function(seq1, seq2,
+                          scorematrix = load_self_sim_mat(),
+                          threshold = Inf) {
+  # Split the sequences into vectors of amino acids
+  peptide.list <- strsplit(x = c(seq1, seq2), split = '')
+
+  # Lookup the score in 'm' function position (p1, p2, ...)
+  score.vec <- mapply(peptide.list[[1]], peptide.list[[2]],
+                      FUN = function(aa1, aa2) scorematrix[aa1, aa2],
+                      USE.NAMES = FALSE)
+
+  # Vector of matches by position
+  p.matches <- peptide.list[[1]] == peptide.list[[2]]
+
+  # Return many stats
+  r <- list(
+    # seq1                      = seq1,
+    # seq2                      = seq2,
+    # change                    = paste(peptide.list[[1]], '->', peptide.list[[2]]),
+    p.matches                = p.matches,
+    score.per.p              = score.vec,
+    total.score              = sum(score.vec[c(3,4,5,6,7,8)]),
+    p5.match                 = p.matches[5],
+    n.mutations              = sum(!p.matches[c(3,4,5,6,7,8)]),
+    n.mutations.p3.and.p4    = sum(!p.matches[c(3,4)]),
+    n.mutations.p6.p7.and.p8 = sum(!p.matches[c(6,7,8)])
+  )
+
+  r$keep.in.list <-
+    (
+      # the total number of mutations within p3-p8 equals 3 or more
+      r$n.mutations >= 3
+      # the total PMBEC score equals 5 or less on p3-p8
+      | r$total.score <= 5
+      # p5 is a mismatch
+      | !r$p5.match
+      # the total # of mutations equals 2 and are both located on the left side
+      # of p5
+      | r$n.mutations >= 2 & r$n.mutations.p3.and.p4 == 2
+      # the total # of mutations equals 2 and are both located on the right
+      # side of p5
+      | r$n.mutations >= 2 & r$n.mutations.p6.p7.and.p8 >= 2
+    )
+  return(r)
+}
+
 
 STSPredictor <- R6::R6Class('STSPredictor',
   inherit = Predictor,
@@ -93,7 +167,8 @@ STSPredictor <- R6::R6Class('STSPredictor',
       super$initialize(hla_allele = hla_allele, verbose = verbose)
     },
     computer = function(peptides, inp_self_peptides = NULL,
-      score_matrix = load_selfsimilarity_matrix()) {
+      score_matrix = load_selfsimilarity_matrix(),
+      use_R = F) {
       ## Persist self_peptides throughout object's lifetime, loading it in can
       ## be costly timewise
       if (!is.null(inp_self_peptides) && is.null(self$self_peptides)) {
@@ -104,8 +179,13 @@ STSPredictor <- R6::R6Class('STSPredictor',
           STS_percentile_rank = self$STS_percentile_rank)
       }
       res <- data.table('peptide' = peptides)
-      res$different_from_self <- match_seq_ext_Rcpp(query_peps = res$peptide,
-        ref_list = self$self_peptides, scorematrix = score_matrix)
+      if (use_R) {
+        res$different_from_self <- match_many_seq_ext(query_peps = res$peptide,
+          ref_list = self$self_peptides, scorematrix = score_matrix)
+      } else {
+        res$different_from_self <- match_seq_ext_Rcpp(query_peps = res$peptide,
+          ref_list = self$self_peptides, scorematrix = score_matrix)
+      }
       return(self$sanitize_res(res))
     }
   ),
